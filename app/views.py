5b13e47d836f2fadcec1032886729a1b20c40b8b
ShambhaviDixit09 <98442140+ShambhaviDixit09@users.noreply.github.com>
2024-01-09 23:33:21 +0530

    files updated

--- app/views.py
+++ app/views.py
@@ -96,4 +96,17 @@ def checkout(request):
 class ProfileView(View):
   def get(self, request):
     form = CustomerProfileForm()
-    return render(request, 'app/profile.html', {'form':form})
+    return render(request, 'app/profile.html', {'form':form, 'active': 'btn-primary'})
+  def post(self, request):
+    form = CustomerProfileForm(request.POST)
+    if form.is_valid():
+      usr = request.user
+      name = form.cleaned_data['name']
+      locality = form.cleaned_data['locality']
+      city = form.cleaned_data['city']
+      state = form.cleaned_data['state']
+      zipcode = form.cleaned_data['zipcode']
+      reg = Customer(user = usr,name = name, locality = locality, city = city, state = state, zipcode = zipcode)
+      reg.save()
+      messages.success(request, 'Profile Updated Successfully!!')
+    return render(request, 'app/profile.html', {'form':form, 'active': 'btn-primary'})
